e8aff254f343c1b5dbf3f19d9f5f95a8762102e5
olegsokur <o.sokur@do-it.co>
2020-02-13 19:11:22 +0400

    Added logs

--- core/bootstrap.go
+++ core/bootstrap.go
@@ -3,19 +3,24 @@ package core
 import (
 	"IOSIF/config"
 	"IOSIF/message_broker"
+	"IOSIF/utils"
+	"fmt"
 )
 
 var broker *message_broker.Manager
 
 func Init() error {
-
+	utils.LogMessage("read config")
 	conf, err := config.ReadConfig("./devops/config.yaml")
 	if err != nil {
 		return err
 	}
 
+	utils.LogMessage("preparing to start broker")
 	broker = message_broker.Init(conf)
+	utils.LogMessage("broker started")
 	broker.StartWorkers()
+	utils.LogMessage("workers started")
 
-	return SetupServer(Router())
+	return SetupServer(Router(), fmt.Sprintf(":%s", conf.Server.Port))
 }
--- core/server.go
+++ core/server.go
@@ -1,10 +1,12 @@
 package core
 
 import (
+	"IOSIF/utils"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func SetupServer(router *mux.Router) error {
-	return http.ListenAndServe(":7000", router)
+func SetupServer(router *mux.Router, port string) error {
+	utils.LogMessage("starting IOSIF server")
+	return http.ListenAndServe(port, router)
 }
